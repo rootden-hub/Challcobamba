69e5208ffcd8b061c70bf0ca8c189657dfd4a322
den <kenyigomezc@gmail.com>
2024-12-18 21:41:22 -0500

    Update ferro-modifcado.py

--- ferro-modifcado.py
+++ ferro-modifcado.py
@@ -306,7 +306,9 @@ def plot_eventos(df, report_date):
     # Etiquetas y título
     ax.set_xlabel('Horas del día')
     ax.set_ylabel('Eventos')
-    ax.set_title(f'Frecuencia de descargas eléctricas por hora del día {report_date}\nSensores Ferrobamba', fontsize=16, pad=20)
+     # Calcular la nueva fecha sumando un día
+    report_date_next_day = add_day_to_date(report_date)
+    ax.set_title(f'Frecuencia de descargas eléctricas por hora del día {report_date} a {report_date_next_day}\nSensores Ferrobamba', fontsize=16, pad=20)
     # Rango de etiquetas: 07:00 a 07:00 del día siguiente
     horas = [f'{(h % 24):02d}:00' for h in range(7, 31)]  # 7 a 30, ajustando al formato de 24 horas
     x_ticks = range(len(horas))  # Posiciones en el eje X
